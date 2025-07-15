e2ffc8239d4b7c7f1b383e7550913d094953346e
SUN TENG <teng_sun@qq.com>
2025-07-15 20:32:43 +0200

    refactor: space to tab

--- modern.config.ts
+++ modern.config.ts
@@ -2,12 +2,12 @@ import { appTools, defineConfig } from '@modern-js/app-tools';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
-  runtime: {
-    router: true,
-  },
-  plugins: [
-    appTools({
-      bundler: 'rspack', // Set to 'webpack' to enable webpack
-    }),
-  ],
+	runtime: {
+		router: true,
+	},
+	plugins: [
+		appTools({
+			bundler: 'rspack', // Set to 'webpack' to enable webpack
+		}),
+	],
 });
